26a49d4f9d1a762975372ee2344fb8f129d53f27
Aslan <quentin.aslan@outlook.com>
2025-04-23 23:53:13 +0200

    :sparkles: Contact Links component created, download resume button implemented.

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,12 @@
   <footer class="flex flex-col md:flex-row items-center justify-between bg-zinc-700 text-white py-4 px-5 lg:px-20 h-auto md:h-40">
     <div class="flex flex-col md:flex-row items-center gap-2 md:gap-4">
       <span class="text-xs lg:text-lg">Copyright © {{ new Date().getFullYear() }}. All rights are reserved.</span>
-      <NuxtLink to="/mentions-legales" class="text-xs lg:text-lg hover:underline">Mentions légales</NuxtLink>
+      <NuxtLink
+        to="/mentions-legales"
+        class="text-xs lg:text-lg hover:underline"
+      >
+        Mentions légales
+      </NuxtLink>
     </div>
     <span class="mt-2 md:mt-0 text-xs lg:text-lg">Developed by <NuxtLink
       to="https://quentinaslan.fr"
